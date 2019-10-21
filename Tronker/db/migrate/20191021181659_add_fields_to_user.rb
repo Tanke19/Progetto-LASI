13517baaffcc3e02e5934add9af5a6f2ae5d0161
Tanke19 <45041145+Tanke19@users.noreply.github.com>
2019-10-21 20:21:31 +0200

    migrate RAILS_ENV=development

--- Tronker/db/migrate/20191021181659_add_fields_to_user.rb
+++ Tronker/db/migrate/20191021181659_add_fields_to_user.rb
@@ -1,4 +1,4 @@
-class AddFieldsToUsers < ActiveRecord::Migration[5.1]
+class AddFieldsToUser < ActiveRecord::Migration[5.1]
   def change
   	add_column :users, :name, :string
   	add_column :users, :username, :string
