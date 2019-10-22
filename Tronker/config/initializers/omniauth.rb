cace964fe31138ffa3b7520abaca2075324345ac
Tanke19 <45041145+Tanke19@users.noreply.github.com>
2019-10-22 16:56:03 +0200

    ID e secret facebook

--- Tronker/config/initializers/omniauth.rb
+++ Tronker/config/initializers/omniauth.rb
@@ -1,10 +1,10 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET']
+    provider :facebook, ENV['444364956186741'], ENV['a0d56a84ae387326ae81032dbfe34d7b']
 end
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, 'my Google client id', 'my Google client secret', {client_options: {ssl: {ca_file:        Rails.root.join("cacert.pem").to_s}}}
+  provider :google_oauth2, '477621880937-8v1hf90lutqcc9tq0hjip0gk3p1vht74.apps.googleusercontent.com', 'my Google client secret', {client_options: {ssl: {ca_file:        Rails.root.join("cacert.pem").to_s}}}
 end    
